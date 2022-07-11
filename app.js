c3dda88d052acfb982e3167cc8d35046505e6b99
Romain-lab <71215457+Romain-lab@users.noreply.github.com>
2022-07-11 21:13:09 +0200

    Update app.js
    Refactoring scroll addeventlistener

--- app.js
+++ app.js
@@ -40,15 +40,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     window.addEventListener("scroll", () => {
         let checkScroll = window.scrollY;
+        let reveals = document.querySelectorAll('.reveal');
+        
         if(checkScroll > 0 && body.classList.value === '') {
             topbar.classList.add('topBarShadow');
         } else {
             topbar.classList.remove('topBarShadow');
         }
-    })
-
-    window.addEventListener('scroll', () => {
-        let reveals = document.querySelectorAll('.reveal');
+        
         for(let i = 0; i < reveals.length; i++) {
             let windowHeight = window.innerHeight;
             let revealTop = reveals[i].getBoundingClientRect().top;
@@ -60,7 +59,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 reveals[i].classList.remove('active');
             }
         }
-    });
+    })
 
     contactForm.addEventListener('submit', function(event) {
         event.preventDefault();
